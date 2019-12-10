17c005060317c2353acebc2320c5266755ff7f92
Bernhard Reisenberger <bernhard@nimbusec.com>
2019-12-10 15:02:48 +0100

    add list domainMetadata

--- domain_metadata.go
+++ domain_metadata.go
@@ -7,6 +7,12 @@ import (
 
 type DomainMetadataService service
 
+func (srv *DomainMetadataService) List(ctx context.Context) ([]DomainMetadata, error) {
+	domainMetadatas := []DomainMetadata{}
+	err := srv.client.Do(ctx, http.MethodGet, "/v3/domains/metadata", nil, &domainMetadatas)
+	return domainMetadatas, err
+}
+
 func (srv *DomainMetadataService) Get(ctx context.Context, id DomainID) (DomainMetadata, error) {
 	domainMetadata := DomainMetadata{}
 	err := srv.client.Do(ctx, http.MethodGet, string(id)+"/metadata", nil, &domainMetadata)
